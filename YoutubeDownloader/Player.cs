b822b12f8c13efdbc6837f5f372455314c790a79
Xyehtz <alejitogarbo@gmail.com>
2023-09-20 17:24:32 -0400

    Download works properly, added a new media player function

---
+++ YoutubeDownloader/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using NAudio.Wave;
+
+namespace MusicPlayer
+{
+    class Program
+    {
+        public static void Player()
+        {
+            var mediaPlayer = new WaveOutEvent();
+            string musicFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonMusic));
+            string[] musicFiles = Directory.GetFiles(musicFolderPath, "*.mp3");
+
+            foreach (string musicFile in musicFiles)
+            {
+                var audioFile = new AudioFileReader(musicFile);
+
+                int lenght = musicFile.Length - 26;
+                string subString = musicFile.Substring(22, lenght);
+                Console.WriteLine($"Now playing {subString}");
+
+                mediaPlayer.Init(audioFile);
+                mediaPlayer.Play();
+
+                Console.ReadKey();
+                mediaPlayer.Dispose();
+            }
+        }
+}
+}
--- YoutubeDownloader/Program.cs
+++ YoutubeDownloader/Program.cs
@@ -1,69 +1,73 @@
 ﻿using System;
 using YoutubeExplode;
 using YoutubeExplode.Videos.Streams;
-using System.IO;
-using YoutubeExplode.Converter;
+using MusicPlayer;
+using CliWrap.EventStream;
 
 namespace Program;
 
-public class Program
+public class Methods
 {
     public static async Task Main(string[] args)
     {
-        Console.WriteLine("Please enter the YouTube link below");
+        Console.WriteLine("Paste the URL of the video to download");
         string youtubeLink = Console.ReadLine();
-
-        var youtube = new YoutubeClient();
-
-        var obtainVideo = await youtube.Videos.GetAsync(youtubeLink);
-
-        string title;
 
         if (youtubeLink.Contains("https://www.youtube.com/watch?v="))
         {
-            title = obtainVideo.Title;
+            var youtube = new YoutubeClient();
+            var obtainVideo = await youtube.Videos.GetAsync(youtubeLink);
+
+            string title = obtainVideo.Title;
             var author = obtainVideo.Author;
             var duration = obtainVideo.Duration;
 
-            Console.WriteLine($"Title:\t\t{title}\nAuthor:\t\t{author}\nDuration:\t{duration}");
+            Console.WriteLine($"\nTitle:\t\t{title}\nAuthor:\t\t{author}\nDuration:\t{duration}");
+
+            Download download = new Download();
+
+            await Download.DownloadMethod(youtube, youtubeLink, title);
         }
         else
         {
             Console.WriteLine("Sorry, but it seems to be a problem with the link you've provided, try to add a new link");
         }
+    }
 
+}
 
+class Download
+{
+    public static async Task DownloadMethod(YoutubeClient youtube, string url, string title)
+    {
+        var video = await youtube.Videos.GetAsync(url);
 
-        var streamManifest = await youtube.Videos.Streams.GetManifestAsync(youtubeLink);
+        var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
         var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
-        //string savePath = "C:\\downloadtest";
-
-        //using (var httpClient = new HttpClient())
-        //{
-        //    var videoStream = await httpClient.GetStreamAsync(streamInfo.Url);
-        //    using (var fileStream = File.OpenWrite(savePath))
-        //    {
-        //        await videoStream.CopyToAsync(fileStream);
-        //    }
-        //}
 
         var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
-        Console.WriteLine(stream);
 
-        string fileName = obtainVideo.Title;
+        await youtube.Videos.Streams.DownloadAsync(streamInfo, Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonMusic), $"{title}.mp3"));
 
-        await youtube.Videos.Streams.DownloadAsync(streamInfo, Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonMusic), $"{fileName}.mp3"));
+        Console.WriteLine($"\n{title} downloaded succesfully on:\t{Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonMusic))}");
 
-        //string origin = "YoutubeDownloader\\bin\\Debug\\net6.0\\video.webm";
-        //string destiny = @"G:\My Drive";
+        while (true)
+        {
+            Console.WriteLine("Do you want to play the songs inside your folder?\nType 1 to play\t\t Type 2 to exit");
+            string number = Console.ReadLine();
 
-        //try
-        //{
-        //    File.Move(origin, destiny);
-        //    Console.WriteLine("Exito");
-        //} catch (IOException e) {
-        //    Console.WriteLine(e);
-        //}
-
+            if (number == "1")
+            {
+                MusicPlayer.Program.Player();
+            }
+            else if (number == "2")
+            {
+                Environment.Exit(1);
+            }
+            else
+            {
+                Console.WriteLine("Error, please follow the instructions");
+            }
+        }
     }
 }
